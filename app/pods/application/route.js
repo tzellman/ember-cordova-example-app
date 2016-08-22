ab324986ce87673275b1f98034575dd4a201dcab
Tom Zellman <tzellman@gmail.com>
2016-08-21 23:46:50 -0400

    Added splash screen and app icon

---
+++ app/pods/application/route.js
@@ -0,0 +1,7 @@
+import Ember from "ember";
+import SplashscreenMixin from "ember-cordova/mixins/device/splashscreen";
+
+const {Route} = Ember;
+
+export default Route.extend(SplashscreenMixin, {
+});
